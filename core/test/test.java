8a7d8c7ae3502595890783c33217d20023570a91
tchuna <elgneramos@gmail.com>
2018-06-03 05:07:00 +0100

    docs

--- core/test/test.java
+++ core/test/test.java
@@ -8,14 +8,4 @@ import static org.junit.Assert.*;
 
 public class test {
 
-    @Test
-    public void isAliveIslander() {
-        PlayScreen screen=new PlayScreen(new MGame(),1);
-        Islander islander=  new Islander(screen);
-        assertTrue(islander.getAlive());
-        islander.setLive(5);
-        assertFalse(islander.getAlive());
-
-    }
-
 }
